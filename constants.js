ec186b7b62d7d0c9628167fb574ff3355f93d9e5
nischit <nischitpra@gmail.com>
2023-09-17 00:38:49 +0545

    minor change

--- constants.js
+++ constants.js
@@ -1,5 +1,9 @@
+const GATEWAY_URL = "https://ipfs.mercle.xyz";
+const IPFS_BASE_API_URL = "https://ipfsrpc.mercle.xyz";
+const IPFS_API_URL = `${IPFS_BASE_API_URL}/api/v0`;
+
 module.exports = {
-  GATEWAY_URL: "https://ipfs.mercle.xyz",
-  IPFS_BASE_API_URL: "https://ipfsrpc.mercle.xyz/api/v0",
-  IPFS_API_URL: `${this.IPFS_BASE_API_URL}/api/v0`,
+  GATEWAY_URL,
+  IPFS_BASE_API_URL,
+  IPFS_API_URL,
 };
