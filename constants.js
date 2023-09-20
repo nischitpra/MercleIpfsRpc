5c959f06f1028999b1f47cd487a372e6992d3fc3
nischit <nischitpra@gmail.com>
2023-09-20 08:02:24 +0545

    update gateway domain

--- constants.js
+++ constants.js
@@ -1,6 +1,6 @@
 const IPFS_CLOUDFRONT_DISTRIBUTION_ID = "E3JNFAV5U2REF6";
 const IPFS_CLOUDFRONT_REGION = "us-east-1";
-const GATEWAY_URL = "https://ipfs.mercle.xyz";
+const GATEWAY_URL = "https://storage.mercle.xyz";
 const IPFS_BASE_API_URL = "https://ipfsrpc.mercle.xyz";
 const IPFS_API_URL = `${IPFS_BASE_API_URL}/api/v0`;
 
