a0d899d5031cc5105edf3139dc2bb49184ac225c
nischit <nischitpra@gmail.com>
2023-12-26 01:25:09 +0530

    set ipns expiary to 100years

--- helpers/ipfsHelper.js
+++ helpers/ipfsHelper.js
@@ -109,7 +109,7 @@ const publishIpfsCid = async ({ keyName, ipfsCid }) => {
   }
 
   const res = await postJson(
-    `${IPFS_API_URL}/name/publish?arg=${ipfsCid}&resolve=false&key=${utils.getKeyName(keyName)}&allow-offline=true`
+    `${IPFS_API_URL}/name/publish?arg=${ipfsCid}&resolve=false&key=${utils.getKeyName(keyName)}&allow-offline=true&lifetime=876000h`
   );
 
   // pin the content just in case its not uploaded from our node
